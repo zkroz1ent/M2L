977bd9302ce2c5d72ec4b73054ca40b260e9879f
zkroz1ent <johndoe@limayrac.fr>
2021-01-19 16:58:50 +0100

    connexion 19/01/2021

---
+++ Connexion.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Connexion</title>
+</head>
+<body>
+    <p><h1>Connexion</h1></p>
+    <p>
+    <label for="">Pseudo</label><br>
+    <input type="text" name="" id="">
+    <br>
+    <br>
+
+    <label for="">Mot de passe</label><br>
+    <input type="password" />
+    
+    <button type="submit" name="moderne">Envoyer</button>
+<button type="submit" name="php">
+
+    </p>
+
+    <input type="button" value="">ok
+</body>
+</html>
