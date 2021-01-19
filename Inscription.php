93313d038de9c588153c3abc9ecaf198f4f5cfe0
CALIFORNIUMM <yohan.marques@limayrac.fr>
2021-01-19 16:57:04 +0100

    inscription

---
+++ Inscription.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>M2L</title>
+</head>
+<body>
+    <h1>M2L</h1>
+    <h2>Inscription</h2>
+
+    <label for="nom">Pseudo</label><br><input type="text"><br><br>
+    <label for="Email">Email</label><br><input type="text"><br><br>
+    <label for="Mot de Passe">Mot de Passe</label><br><input type="text"><br><br>
+    <a href="">Enregistrer</a>
+    <a href="">Annuler</a>
+</body>
+</html>
