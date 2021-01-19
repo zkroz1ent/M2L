2070a12c4a7f40df6a533ded92f58bc8d1ffe477
CALIFORNIUMM <yohan.marques@limayrac.fr>
2021-01-19 16:30:58 +0100

    test

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="css/main.css">
 </head>
 <body>
     Lorem ipsum, dolor sit amet consectetur adipisicing elit. At voluptatum, quo ducimus sunt ratione, doloribus delectus cum laborum dolore nam labore dolorem quod ea iste recusandae beatae itaque necessitatibus eaque!
