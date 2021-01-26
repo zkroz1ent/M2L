0cd947872f424242330fc0740fc8e2ce045f2fa2
CALIFORNIUMM <yohan.marques@limayrac.fr>
2021-01-26 14:16:06 +0100

    Deco / Inscription

---
+++ Deconnexion.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Déconnexion</title>
+    <link rel="stylesheet" href="css/main.css">
+</head>
+<body>
+    <h1>M2L</h1>
+    <h2>Déconnexion</h2>
+    
+</body>
+</html>
--- Inscription.php
+++ Inscription.php
@@ -1,9 +1,10 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>M2L</title>
+    <title>Inscription</title>
+    <link rel="stylesheet" href="css/main.css">
 </head>
 <body>
     <h1>M2L</h1>
@@ -12,6 +13,14 @@
     <label for="nom">Pseudo</label><br><input type="text"><br><br>
     <label for="Email">Email</label><br><input type="text"><br><br>
     <label for="Mot de Passe">Mot de Passe</label><br><input type="text"><br><br>
+    <label for="Ligue">Ligue</label><br>
+    <select name="ligue" id="ligue-select">
+        <option value="">--Please choose an option--</option>
+        <option value="foot">Football</option>
+        <option value="basket">BasketBall</option>
+        <option value="handball">Handball</option>
+        <option value="volley">Volley</option>
+    </select><br><br>
     <a href="">Enregistrer</a>
     <a href="">Annuler</a>
 </body>
