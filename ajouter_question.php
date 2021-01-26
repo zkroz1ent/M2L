b1e9dbedd25eda93542562735e42a3007231b184
zkroz1ent <johndoe@limayrac.fr>
2021-01-26 14:28:15 +0100

    26/01/2021

---
+++ ajouter_question.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+ <a  href="Deconnexion.php">deconnecter</a>
+
+
+
+
+
+<p><H1>M2L</H1></p>
+<p><H3>Ajouter une question</H3></p>
+
+
+
+<p>
+<label for="posequestion">Question</label> <br>  
+<textarea name="posequestion" id="posequestion" cols="40" rows="20"></textarea><br> <br>
+
+<button type="submit" name="moderne">Enregister</button> &nbsp  &nbsp&nbsp <a <button type="submit" name="moderne"  href="Page_accueil.php">annuler</button></a>
+
+
+
+
+
+</p>
+
+
+</body>
+</html>
