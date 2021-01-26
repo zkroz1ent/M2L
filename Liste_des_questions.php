7eaacc32363350629c00cc6d4cdbe1841c95d742
AdrienCarles <adrien.carles@limayrac.fr>
2021-01-26 14:28:55 +0100

    Liste question 26/01/2021

---
+++ Liste_des_questions.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Liste des questions</title>
+</head>
+<body>
+    <h1>Liste des questions</h1>
+    <br><br>
+    <table>
+        <tr>
+            <th>Nr</th>
+            <th>Auteur</th>
+            <th>Questions</th>
+            <th>Réponse</th>
+            <th>Action</th>
+        </tr>
+        <tr>
+            <td>1</td>
+            <td>userfoot</td>
+            <td>Quelle est la taille d une cage de foot ?</td>
+            <td>Dimension d'un but de football à 11 : 7,32 x 2,44 m</td>
+            <td>// lien a rentrer </td>
+        </tr>
+        <tr>
+            <td>2</td>
+            <td>userhand</td>
+            <td>Quelle est la durée d un match de handball ?</td>
+            <td>Deux mi-temps de 30 minutes</td>
+            <td>// lien a rentrer </td>
+        </tr>
+        <tr>
+            <td>3</td>
+            <td>userhand</td>
+            <td>Quel est le nombre de joueurs dans une équipe de handball</td>
+            <td></td>
+            <td>// lien a rentrer </td>
+        </tr>
+    </table>
+    <p><a href="ajouter_question.php">Nouvelle Question</a></p>
+</body>
+</html>
