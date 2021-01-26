5aec689666ab4bcc6487f808ab7c662317d938b9
zkroz1ent <johndoe@limayrac.fr>
2021-01-26 14:40:20 +0100

    26/01/2021

---
+++ supprimer.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Supprimer</title>
+</head>
+<body>
+
+  
+
+<p> <a  href="Deconnexion.php">deconnecter</a>
+
+
+
+
+
+<p><H1>M2L</H1></p>
+<p><H3>Supprimer une question</H3></p>
+
+
+
+<p>
+<label for="posequestion">Question</label> <br>  
+<textarea name="posequestion" id="posequestion" cols="40" rows="20"></textarea><br> <br>
+<br>
+<br>
+
+
+<label for="Repquestion">Reponse</label> <br>  
+<textarea  name="Repquestion" id="Repquestion" cols="40" rows="20"></textarea><br> <br>
+<br>
+<button type="submit" name="moderne">Enregister</button> &nbsp  &nbsp&nbsp <a <button type="submit" name="moderne"  href="Liste_des_questions.php">annuler</button></a></p>
+
+
+
+
+
+</body>
+</html>
