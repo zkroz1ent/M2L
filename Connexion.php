f0d13fe4c910c8c3efa7de4e8cf6dfc114cc1f80
CALIFORNIUMM <yohan.marques@limayrac.fr>
2021-01-26 14:47:59 +0100

    t

--- Connexion.php
+++ Connexion.php
@@ -7,24 +7,27 @@
     <link rel="stylesheet" href="css/main.css">
 </head>
 <body>
-<p><h1>M2L</h1></p>
-    <p><h2>Connexion</h2></p>
-    
-    <p>
-    <label for="">Pseudo</label><br>
-    <input type="text" name="" id="">
-    <option value="Pseudo"></option>
-    <br>
-    <br>
+<div class="marg">
+    <h1>M2L</h1>
+        <p><h2>Connexion</h2></p>
+        
+        <p>
+        <label for="">Pseudo</label><br>
+        <input type="text" name="" id="">
+        <option value="Pseudo"></option>
+        <br>
+        <br>
 
-    <label for="">Mot de passe</label><br>
-    <input type="password" />
-    <option value="password"></option>
-    <br>
-    <button type="submit" name="moderne">accepter</button> &nbsp  &nbsp <a <button type="submit" name="moderne"  href="Page_accueil.php">retour</button></a>
+        <label for="">Mot de passe</label><br>
+        <input type="password" />
+        <option value="password"></option>
+        <br>
+        <button type="submit" name="moderne">accepter</button> &nbsp  &nbsp <a <button type="submit" name="moderne"  href="Page_accueil.php">retour</button></a>
 
-    <option value="Annuler"></option>
+        <option value="Annuler"></option>
 
+
+</div>
 
 
 
