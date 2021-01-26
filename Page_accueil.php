555cbf15c9935f931aede274369469d4ea80b159
CALIFORNIUMM <yohan.marques@limayrac.fr>
2021-01-26 14:40:05 +0100

    css/navbar

--- Page_accueil.php
+++ Page_accueil.php
@@ -7,13 +7,20 @@
     <link rel="stylesheet" href="css/main.css">
 </head>
 <body>
-    <h1>Maison des ligues de Lorraine</h1>
-    <h2>Accueil</h2>
-    <h3>Bienvenue  sur la FAQ da la maison des ligues de Lorraine</h3>
-    <p><img src="Img\mdl.png" alt="FATAL ERROR"></p>
-    <input type="submit" value="Inscription" /> &nbsp &nbsp     
-    <input type="submit" value="Connexion" /> 
-    <br><br><br><br><br>
-    <p>SIO 2020/2021 Marques, Dutertre, Carles</p>
+    <ul>
+        <li><a href="Liste_des_questions.php">FAQ</a></li>
+        <li class="right"><a  href="Inscription.php">S'inscrire</a></li>
+        <li class="right" ><a href="Connexion.php">Se connecter</a></li>
+       
+    </ul>
+
+    <div class="marg">
+        <h1>Maison des ligues de Lorraine</h1>
+        <h2>Accueil</h2>
+        <h3>Bienvenue  sur la FAQ da la maison des ligues de Lorraine</h3>
+        <p><img src="Img\mdl.png" alt="FATAL ERROR"></p>
+        <br><br><br><br><br>
+        <p>SIO 2020/2021 Marques, Dutertre, Carles</p>
+    </div>
 </body>
 </html>
