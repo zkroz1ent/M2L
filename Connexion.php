9fbbe182fcf2bd444f69ce9c296696b5e546ea54
zkroz1ent <johndoe@limayrac.fr>
2021-01-26 14:12:29 +0100

    26/01/2021

--- Connexion.php
+++ Connexion.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Connexion</title>
+    <link rel="stylesheet" href="css/main.css">
 </head>
 <body>
 <p><h1>M2L</h1></p>
@@ -12,24 +13,24 @@
     <p>
     <label for="">Pseudo</label><br>
     <input type="text" name="" id="">
+    <option value="Pseudo"></option>
     <br>
     <br>
 
     <label for="">Mot de passe</label><br>
     <input type="password" />
+    <option value="password"></option>
     <br>
-    <br>
-    <button type="submit" name="moderne">ok</button>
+    <button type="submit" name="moderne">accepter</button> &nbsp  &nbsp <a <button type="submit" name="moderne"  href="Page_accueil.php">retour</button></a>
 
-&nbsp;
-&nbsp;
-&nbsp;
+    <option value="Annuler"></option>
 
-<button type="submit" name="moderne">Annuler</button>
+
+
+
 
 
 
  </p>
-   
 </body>
 </html>
