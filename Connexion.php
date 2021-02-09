021b0f061ef4e8e7677c4b7d2c3c71ad8ed6ae64
AdrienCarles <adrien.carles@limayrac.fr>
2021-02-09 14:10:15 +0100

    Mise en formulaire de connexion

--- Connexion.php
+++ Connexion.php
@@ -16,30 +16,17 @@
 <div class="marg">
     <h1>M2L</h1>
         <p><h2>Connexion</h2></p>
-        
-        <p>
-        <label for="">Pseudo</label><br>
-        <input type="text" name="" id="">
-        <option value="Pseudo"></option>
-        <br>
-        <br>
-
-        <label for="">Mot de passe</label><br>
-        <input type="password" />
-        <option value="password"></option>
-        <br>
-        <button type="submit" name="moderne"><a href="Liste_des_questions.php">accepter</a></button> &nbsp  &nbsp <button button type="submit" name="moderne"><a href="Page_accueil.php">retour</a></button>
-
-        <option value="Annuler"></option>
-
-
+        <h3>Veuillez remplir le formulaire</h3>
+        <form action="formulaireconnexion.php" method="post">
+            <label for="pseudo">Pseudo</label><br>
+            <input type="text" name="pseudo"/>
+            <br><br>
+            <label for="mdp">Mot de passe</label><br>
+            <input type="password" name="mdp"/>
+            <br><br>
+            <input type="submit" name="submit" value="Enregistrer"> &nbsp  &nbsp 
+            <input type="reset" name="submit" value="Réinitialiser">
+        </form>
 </div>
-
-
-
-
-
-
- </p>
 </body>
 </html>
