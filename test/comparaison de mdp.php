d6001f2b2eb51a4e8229a173bb5434b71883f3dd
zkroz1ent <johndoe@limayrac.fr>
2021-02-09 15:22:16 +0100

    ui

--- test/comparaison de mdp.php
+++ test/comparaison de mdp.php
@@ -28,11 +28,10 @@ while ($donnees = $reponse->fetch())
 {
 ?>
     <p>
-    <strong>Jeu</strong> : <?php echo $donnees['nom']; ?><br />
-    Le possesseur de ce jeu est : <?php echo $donnees['possesseur']; ?>, et il le vend à <?php echo $donnees['prix']; ?> euros !<br />
-    Ce jeu fonctionne sur <?php echo $donnees['console']; ?> et on peut y jouer à <?php echo $donnees['nbre_joueurs_max']; ?> au maximum<br />
-    <?php echo $donnees['possesseur']; ?> a laissé ces commentaires sur <?php echo $donnees['nom']; ?> : <em><?php echo $donnees['commentaires']; ?></em>
-   </p>
+    <?php echo $donnees['nbre_joueurs_max']; ?>
+
+
+    
 <?php
 }
 
