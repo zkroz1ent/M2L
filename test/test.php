91f8e971cfbe0afea54df3ecd1d4761a76d855ea
zkroz1ent <johndoe@limayrac.fr>
2021-03-02 16:52:15 +0100

    insertion bdd

--- test/test.php
+++ test/test.php
@@ -8,33 +8,41 @@
 <body>
 
 
-<?php
 
-try
-{
-	$dbh = new PDO('mysql:host=localhost;dbname=ligue des sport;charset=utf8', 'root', '');
-}
-catch(Exception $e)
-{
-        die('Erreur : '.$e->getMessage());
+
+
+<?php
+$id_usertype=1;
+$nom='test';   
+$passe='test';  
+$Email='test@m2l.fr';
+$id_usertype=1 ; 
+$ligue=5;
+$id_user=9;
+
+$dsn ='mysql:host=localhost;dbname=ligue des sport'; 
+$user ='root';
+$password = '';
+try {
+$dbh = new PDO($dsn, $user, $password, array(PDO::MYSQL_ATTR_INIT_COMMAND =>"SET NAMES utf8"));
+$dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+} catch (PDOException $ex) {
+die("Erreur lors de la connexion SQL : " . $ex->getMessage());
 }
 ?>
 
 <?php
-$id_usertype=1;
-                    
-                    
-                   
-$req = $dbh->prepare('INSERT INTO user(pseudo, mpd, mail, id_usertype ,id_ligue) VALUES(:pseudo, :mpd, :mail, :id_usertype, :id_ligue)');
-$req->execute(array(
-    
-    'pseudo' =>  $nom,
-    'mpd' =>  ' password_hash( $_POST["passe"], PASSWORD_DEFAULT)',
-    'mail'=>   '$_POST["Email"]',
-    'id_usertype'=> '$id_usertype' ,
-    'id_ligue'=>   '$_POST["ligue"]'
-    
+echo'cheh';
 
+$req = $dbh->prepare('INSERT INTO user(id_user,pseudo, mdp, mail, id_usertype, id_ligue) VALUES(:id_user,:pseudo, :mdp, :mail, :id_usertype, :id_ligue)');
+$req->execute(array(
+    'id_user'  => $id_user,
+    'pseudo' => $nom,
+    'mdp' => $passe,
+    'mail'=>   $Email,
+    'id_usertype'=> $id_usertype,
+    'id_ligue'=>   $ligue 
     ));
 
 echo 'enregistrement effectuéé !';
